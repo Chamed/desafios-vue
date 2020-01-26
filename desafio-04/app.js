494c49258221e7eab5b177b6559a635d7355b2a3
Chamed <christianrique@gmail.com>
2020-01-26 16:43:29 -0300

    desafio-04

---
+++ desafio-04/app.js
@@ -0,0 +1,435 @@
+new Vue({
+	el: '#app',
+	data: {
+		produtos: [
+			{
+				_id: '22qR486p83xa6KAsW',
+				descricao: 'Sacola Pequena 30x40',
+				idCategoria: 'gCZxfiCZ3Trd8mcA7',
+				idUnidade: '3kunu9uqrgWKDALrN',
+				horario: '',
+				prazo: '',
+				qtdMinima: '1',
+				diasSemana: [
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6'
+				],
+				valor: 29,
+				_groupId: 'bAar8TkTwuysheuay',
+				produtoEstoque: false,
+				updatedAt: '2018-12-12T01:16:30.919Z',
+				fatorConversao: '1'
+			},
+			{
+				_id: '22wiyG5v5gjPTaBNk',
+				descricao: 'Luva Procedimento Vinil Tam G sem talco / cx',
+				descricaoLower: 'luva procedimento vinil / cx',
+				preco: '',
+				idCategoria: 'fY4rjbQyxB3BFvAuA',
+				ativo: true,
+				_groupId: 'Ey396XTp3BX893F7J',
+				valor: 0,
+				produtoEstoque: false,
+				updatedAt: '2018-12-12T01:16:30.919Z',
+				fatorConversao: '1',
+				qtdMinima: '1',
+				diasSemana: [
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6'
+				]
+			},
+			{
+				_id: '24Mej4hFBk39Rum45',
+				descricao: 'Vodka Orloff 1L',
+				codigo: 791,
+				idCategoria: 'AdcDE2kHiveLNDhGo',
+				idUnidade: 'XGGFKxBp4fqT4jEC7',
+				valor: 23.66,
+				qtdMinima: '1',
+				produtoEstoque: false,
+				horario: '',
+				prazo: '',
+				idFornecedores: [
+					'edbL4WSgMP7cYRgaY'
+				],
+				diasSemana: [
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6'
+				],
+				conteudoProduto: '',
+				skus: [],
+				skusVariacoes: [],
+				descontoTemporario: {
+					ativo: false
+				},
+				_groupId: 'eq4cq6RqoscmWNsX6',
+				createdAt: '2019-04-03T19:14:11.880Z',
+				updatedAt: '2019-04-03T19:14:11.880Z',
+				fatorConversao: '1'
+			},
+			{
+				_id: '24rMP3QqQq3DG2SFm',
+				descricao: 'Espinafre Folhas Golden 2,5kg',
+				idCategoria: '96S74Ci2x7GqtS6pA',
+				idUnidade: 'JZeT79YshELJ4YCxK',
+				horario: '',
+				prazo: '',
+				qtdMinima: '1',
+				diasSemana: [
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6'
+				],
+				valor: 87.92,
+				idFornecedores: [
+					'gsb9AFJ8hW8kBhC3e'
+				],
+				skus: [],
+				skusVariacoes: [],
+				codigo: 29,
+				_groupId: 'tnHYfyXnp5qPRfxPA',
+				produtoEstoque: false,
+				updatedAt: '2018-12-12T01:16:30.919Z',
+				fatorConversao: '1'
+			},
+			{
+				_id: '25JFwve25v6vb5wSX',
+				descricao: 'Purê de Batatas Instantâneo Qualimax (pacote de 800 g)',
+				idCategoria: 'HMmMdCEuXBdiX55tK',
+				_groupId: 'tnHYfyXnp5qPRfxPA',
+				produtoEstoque: false,
+				updatedAt: '2018-12-12T01:16:30.919Z',
+				fatorConversao: '1',
+				qtdMinima: '1',
+				idFornecedores: [
+					'4uGtiaDBntPrXytPj'
+				],
+				diasSemana: [
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6'
+				]
+			},
+			{
+				_id: '25KrmZNyTBthqQoKc',
+				descricao: 'SACOLA DE PAPEL M - 40x34x18',
+				codigo: '2712',
+				fatorConversao: '1',
+				valor: 3.24,
+				produtoEstoque: false,
+				qtdMinima: '80',
+				idFornecedores: [
+					'p8zf8fGotvMKfMAjR'
+				],
+				idUnidade: 'N3TdP7FvXuEJt3A8M',
+				idCategoria: 'JNA2WLrWuqmgHcwWh',
+				_groupId: '5Ck5k9XjW9xkHvfWW',
+				createdAt: '2019-09-16T20:23:21.644Z',
+				updatedAt: '2019-12-13T14:27:18.719Z',
+				conteudoProduto: '',
+				descontoTemporario: {
+					ativo: false
+				},
+				diasSemana: [
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6'
+				],
+				historicoProduto: [
+					{
+						idUsuario: '9vKLnFxuRX7R4NTcd',
+						dataAlteracao: '2019-09-16T20:40:11.350Z',
+						descricao: 'SACOLA DE PAPEL M - 40x34x18',
+						valor: '3.24',
+						novaDescricao: 'SACOLA DE PAPEL M - 40x34x18',
+						novoValor: 3.24
+					},
+					{
+						idUsuario: '9vKLnFxuRX7R4NTcd',
+						dataAlteracao: '2019-12-13T14:27:18.333Z',
+						descricao: 'SACOLA DE PAPEL M - 40x34x18',
+						valor: 3.24,
+						novaDescricao: 'SACOLA DE PAPEL M - 40x34x18',
+						novoValor: 3.24
+					}
+				],
+				horario: '',
+				idUnidadeEntrada: null,
+				prazo: '',
+				qtdMaxima: '',
+				skus: [],
+				skusVariacoes: [],
+				foto: 'attTK6Xh5vezdykNK'
+			},
+			{
+				_id: '25jf5HCcATpbQ5DTN',
+				descricao: 'COPO TÉRMICO 110 ML',
+				valor: 400,
+				produtoEstoque: true,
+				qtdMinima: 1,
+				idCategoria: 'gHGzxR3mqHDmk456M',
+				idFornecedores: [
+					'bcviLFQfn2THQZLPp'
+				],
+				idUnidade: 'BzfP7M95W2SfjeNKi',
+				codigo: 10121,
+				_groupId: '5Ck5k9XjW9xkHvfWW',
+				createdAt: '2019-01-30T16:17:21.344Z',
+				updatedAt: '2019-01-30T16:17:21.344Z',
+				fatorConversao: '1',
+				diasSemana: [
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6'
+				]
+			},
+			{
+				_id: '25pcWpTMvWqvTB9kr',
+				descricao: 'BONECO DE NEVE BRANCO 90G ',
+				idCategoria: 'zmEstH2MGpe2RrNgA',
+				idUnidade: 'pgwdqPvjjQoYXnNFs',
+				prazo: 'varia de acordo com a região',
+				qtdMinima: '1',
+				produtoEstoque: true,
+				codigo: '8631',
+				diasSemana: [
+					'1',
+					'2',
+					'3',
+					'4',
+					'5'
+				],
+				valor: 106.4,
+				idFornecedores: [
+					'GKhQpLvpNZby6jT2b'
+				],
+				skus: [],
+				skusVariacoes: [],
+				descontoTemporario: {
+					ativo: false
+				},
+				_groupId: '5Ck5k9XjW9xkHvfWW',
+				foto: 'Af8YmNDisCogkxKhe',
+				historicoProduto: [
+					{
+						idUsuario: 'Sm3HYp2tBkRpT4fGw',
+						dataAlteracao: '2018-09-06T13:02:25.271Z',
+						descricao: 'BONECO DE NEVE BRANCO 90G ',
+						valor: 106.4,
+						novaDescricao: 'BONECO DE NEVE BRANCO 90G ',
+						novoValor: 106.4
+					}
+				],
+				updatedAt: '2019-09-16T20:28:08.917Z',
+				fatorConversao: '1'
+			},
+			{
+				_id: '25pwyAXHf7BrEqqKD',
+				descricao: 'TEMPERO PARA BATATA C 2 KG ',
+				codigo: '120102',
+				idCategoria: 'evuXDpJrqcGCMsxdr',
+				idUnidade: 'RoZk7YmSGnk6w3tts',
+				valor: 21.2,
+				qtdMinima: '1',
+				produtoEstoque: false,
+				horario: '',
+				prazo: '',
+				idFornecedores: [
+					'htSPnBN4MrcNL7naD'
+				],
+				diasSemana: [
+					'0',
+					'1',
+					'3',
+					'5',
+					'6'
+				],
+				conteudoProduto: '',
+				skus: [],
+				skusVariacoes: [],
+				descontoTemporario: {
+					ativo: false
+				},
+				_groupId: '5mn53XuQ9NJaQZmwT',
+				createdAt: '2019-03-21T18:44:52.917Z',
+				updatedAt: '2019-12-02T14:07:56.410Z',
+				historicoProduto: [
+					{
+						idUsuario: 'PdFYKBJmWKbXxYR88',
+						dataAlteracao: '2019-04-03T18:34:04.664Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 'GceG2h7e339NkBeQY',
+						dataAlteracao: '2019-04-18T14:06:56.605Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-06-03T15:41:18.204Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-06-03T15:41:18.255Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-07-16T21:22:47.071Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-07-16T21:22:47.103Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-07-18T16:15:37.192Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-07-30T12:49:06.039Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-09-27T20:51:14.006Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-11-05T13:34:34.801Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-11-13T13:50:46.801Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-11-27T13:27:04.156Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					},
+					{
+						idUsuario: 's3fS3tHSLnexPaAec',
+						dataAlteracao: '2019-12-02T14:07:56.409Z',
+						descricao: 'TEMPERO PARA BATATA C 2 KG ',
+						valor: 21.2,
+						novaDescricao: 'TEMPERO PARA BATATA C 2 KG ',
+						novoValor: 21.2
+					}
+				],
+				fatorConversao: '1',
+				unidadeEntrada: '',
+				idUnidadeEntrada: null,
+				qtdMaxima: ''
+			},
+			{
+				_id: '2622SKPmdGwtak4q4',
+				descricao: 'Balas de mel com própolis / pc 500g',
+				codigo: 100,
+				idCategoria: 'v3P97dfQaDANCScKW',
+				idUnidade: 'GStytvTr4otEmMnpw',
+				valor: 32,
+				qtdMinima: '1',
+				produtoEstoque: false,
+				horario: '',
+				prazo: '',
+				idFornecedores: [
+					'otBGy7oHpYETFh8Gv'
+				],
+				diasSemana: [
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6'
+				],
+				conteudoProduto: '',
+				fatorConversao: '1',
+				unidadeEntrada: '',
+				skus: [],
+				skusVariacoes: [],
+				descontoTemporario: {
+					ativo: false
+				},
+				_groupId: 'Ey396XTp3BX893F7J',
+				createdAt: '2019-05-13T21:07:47.306Z',
+				updatedAt: '2019-05-13T21:07:47.306Z'
+			}
+		],
+	}
+})
